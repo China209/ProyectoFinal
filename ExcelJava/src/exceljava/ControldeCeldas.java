f09c721df7bc6eea014563a8bc6077ad3b81fa03
China209 <you@example.com>
2019-05-24 07:51:22 -0600

    Excel en Java con filas y columnas nombradas

---
+++ ExcelJava/src/exceljava/ControldeCeldas.java
@@ -0,0 +1,23 @@
+/*Este código fue realizado por Ester Guamuch y Cyndi Medina*/
+package exceljava;
+
+import java.awt.Color; 
+import java.io.Serializable;
+
+public class ControldeCeldas extends Object implements Serializable, Cloneable {
+        //Variables globales que establecemos como predeterminadas para las celdas
+        public Color cBlanco = Color.white;
+        public Color cGris = Color.gray;
+        public String sCadena = "";
+
+        @Override
+        //Se retorna el valor en cadena del contenido de las celdas
+        public String toString() {
+            return sCadena;
+        }
+
+        @Override//Nos permite "clonar" lo que en las celdas existe con sus propiedades
+        public  Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+    }
